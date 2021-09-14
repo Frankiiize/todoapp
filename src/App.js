102041e072d0cc3f8d47483b32968ef995857ec1
Francisco <frankiiize.gmail.com>
2021-09-14 11:18:13 -0300

    estilos al header y al input de crear TO-DO's

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
-import './App.css';
+import './sass/App.scss';
+import sun from './images/icon-sun.svg';
+import check from './images/icon-check.svg';
+import cross from './images/icon-cross.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="headerContainer">
+        <h1>TODO</h1>
+        <img src={sun} />
       </header>
-    </div>
+
+        <label className="searchContainer">
+          <input value="" type="submit" className="searchContainer__button"/>
+          <input
+          placeholder="Create a new TODO" 
+          className="searchContainer__input" />
+          
+        </label>
+
+
+      <section>
+        <div>
+          <img src={check}/>
+          <p>Primer TODO</p>
+          <img src={cross} />
+        </div>
+      </section>
+
+
+     {/*  <section>
+        <p>All</p>
+        <p>Active</p>
+        <p>Complete</p>
+      </section>
+
+      <footer>
+        Drag and drop to reorder list
+      </footer> */}
+    </>
+
+   
   );
 }
 
